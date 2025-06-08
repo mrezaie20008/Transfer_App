bdcd2316dfa7aa787e06cd6bff2ca6e6f5a368d8
Mohammad Rezaie <mohammadrezaie20081387@gmail.com>
2025-06-08 11:50:20 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from socket import socket as sk, AF_INET, SOCK_STREAM
+from requests import get
+
+# IP Reveal
+print(f"IP: {get("https://api.ipify.org")}")
+
+# Server Code
+serv = sk(AF_INET, SOCK_STREAM)
+serv.bind(("0.0.0.0", 12345))
+
+client, _ = serv.accept()
+
+print(client.recv(1024).decode())
+
+serv.send("SC; BYE!".encode())
+
+client.close()
+serv.close()
